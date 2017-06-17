d526cb13fb25d36d97dfd2d02bce3883eda7b93f
Faiza <faizabeauty22@gmail.com>
2017-06-17 14:23:29 +0000

    entered missing data

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '@xr6=@s(kvsc)%$&4xlsjzg&w=$x3jn+783y2maw48c+ws_7=o'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1',	'.pythonanywhere.com']
 
 
 # Application definition
